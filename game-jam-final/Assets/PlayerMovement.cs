3d6e8372db87ef6751bf53bd82341ed8944463d5
AJ Aguiar <ajaguiar1007@gmail.com>
2021-03-20 17:42:47 -0400

    changies

--- game-jam-final/Assets/PlayerMovement.cs
+++ game-jam-final/Assets/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+	[SerializeField] private LayerMask platformLayerMask;
+    Rigidbody2D rb;
+	BoxCollider2D boxCollider2d;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+       rb = transform.GetComponent<Rigidbody2D>(); 
+	   boxCollider2d = transform.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
+		IsGrounded();
     }
+	    void Jump() 
+    { 
+        if (Input.GetKeyDown(KeyCode.Space) && (IsGrounded())) {
+            float jumpVelocity = 10f;
+			rb.velocity = Vector2.up * jumpVelocity;
+    } 
 }
+	bool IsGrounded() {
+		RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platformLayerMask);
+		return raycastHit2d.collider != null;
+	}
+}
+
